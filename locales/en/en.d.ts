92648ecdab13d60aa8426f47b8356645e46978d3
unknown <rosyrdas@gmail.com>
2022-11-14 13:47:06 +0530

    feat: add locales

---
+++ locales/en/en.d.ts
@@ -0,0 +1,7 @@
+export declare type i18next = {
+  errors: {
+    interactions: {
+      unauthorized_member: string;
+    };
+  };
+};
---
+++ locales/en/en.js
@@ -0,0 +1,7 @@
+module.exports = {
+    errors: {
+        interactions: {
+            unauthorized_member: "This is not for you.",
+        }
+    }
+}
